09524fac562ec54babc2cea10d5eb154971f218f
Bruno-Delourme <delourmebruno@gmail.com>
2025-08-07 09:45:01 +0200

    erase user never used on about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { User, GraduationCap, Briefcase, Award } from 'lucide-react';
+import { GraduationCap, Briefcase, Award } from 'lucide-react';
 
 const About = () => {
   return (
